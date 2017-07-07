2865ad0db935418f567fede5ca24144cad9d77e4
root <root@node3.example.com>
2017-07-07 16:12:24 +0800

    add function

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+int a=0,b=0;
+void mydef(a,b)
+{
+	int c=0;
+	c=a+b;
+	printf("add result is %d\n",c);
+}
+
 int main()
 {
 	printf("hello world\n");
+	mydef(9,10);
 	return 0;
 
 }
+
