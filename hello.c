d84512fdc1243979da6e254e036b8b55e55d99be
13644118303@163.com <13644118303@163.com>
2017-07-10 15:30:31 +0800

    struct

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@
  *Email:**********
  *This is a test git code
  */
-
+/*
 #include <unistd.h>
 #include<stdio.h>
 int a=0,b=0;
@@ -42,13 +42,41 @@ void not7()
 	printf("%d\n",a);
 	}
 }
+
+struct stuff{
+	char name[10];
+	int age;
+	float score;
+};
+
 int main()
 {
 	printf("hello world\n");
-	mydef(9,10);
-	jc();
-	not7();
+	//mydef(9,10);
+	//jc();
+	//not7();
+	struct stuff students={"xiao",28,99};
+	printf("%s%d%f\n",students.name,students.age,students.score);
 	return 0;
 
 }
+*/
 
+
+
+#include <stdio.h> 
+ 
+struct student  //结构体类型的说明与定义分开。 声明
+{
+int age;   /*年龄*/
+float score;  /*分数*/
+char sex;     /*性别*/
+};
+int main ()
+ 
+{
+ struct student a={ 20,79,'f'}; //定义
+printf("年龄：%d 分数：%.2f 性别：%c\n", a.age, a.score, a.sex  );
+ 
+return 0;
+}
